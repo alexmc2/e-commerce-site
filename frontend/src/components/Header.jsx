0f9de2a0897eee2842535c6d93ba374aec19bff5
Alex <alex_mcgarry@hotmail.co.uk>
2023-07-29 14:46:59 +0100

    add link containers

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,37 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../../src/assets/logo.png'
+import logo from '../../src/assets/logo.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="shop" />{' '}
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="shop" />{' '}
+              <span
+                className="ms-3"
+                style={{ fontFamily: 'Poppins', fontSize: '23px' }}
+              >
+                THE WOW ZONE
+              </span>
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link >
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,22 +1,22 @@
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
-
   const formattedPrice = product.price.toLocaleString('en-GB', {
     style: 'currency',
     currency: 'GBP',
   });
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           <div className="my-3">
@@ -24,7 +24,7 @@ const Product = ({ product }) => {
           </div>
         </Card.Text>
 
-          <Card.Text as="h3">{formattedPrice}</Card.Text>
+        <Card.Text as="h3">{formattedPrice}</Card.Text>
       </Card.Body>
     </Card>
   );
