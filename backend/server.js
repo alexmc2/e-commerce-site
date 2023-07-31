dbb62957305295d603a5fd536816526167bffd3b
Alex <alex_mcgarry@hotmail.co.uk>
2023-07-31 18:51:19 +0100

    set up routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import products from './data/products.js';
+
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 connectDB(); // connect to MongoDB
 
@@ -14,13 +15,6 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
