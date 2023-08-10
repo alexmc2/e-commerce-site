f0656b5aaf54d3c63d38be9b897fdb77e1ffe6dc
Alex <alex_mcgarry@hotmail.co.uk>
2023-08-10 15:57:59 +0100

    add checkout steps

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../slices/cartSlice';
 import { useNavigate } from 'react-router-dom';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -27,6 +27,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 step3 step4 />
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
